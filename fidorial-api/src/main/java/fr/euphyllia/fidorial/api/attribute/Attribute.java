5fd3cc30e7deff51c701ed4a4ea32a2bd92adf00
Euphyllia Bierque <bierque.euphyllia@gmail.com>
2026-07-17 16:09:47 +0200

    Prepare NBT ItemStack

---
+++ fidorial-api/src/main/java/fr/euphyllia/fidorial/api/attribute/Attribute.java
@@ -0,0 +1,51 @@
+package fr.euphyllia.fidorial.api.attribute;
+
+import fr.euphyllia.fidorial.api.registry.Key;
+
+public class Attribute {
+
+    public static final Key MAX_HEALTH = Key.minecraft("max_health");
+    public static final Key FOLLOW_RANGE = Key.minecraft("follow_range");
+    public static final Key KNOCKBACK_RESISTANCE = Key.minecraft("knockback_resistance");
+    public static final Key MOVEMENT_SPEED = Key.minecraft("movement_speed");
+    public static final Key FLYING_SPEED = Key.minecraft("flying_speed");
+    public static final Key ATTACK_DAMAGE = Key.minecraft("attack_damage");
+    public static final Key ATTACK_KNOCKBACK = Key.minecraft("attack_knockback");
+    public static final Key ATTACK_SPEED = Key.minecraft("attack_speed");
+    public static final Key ARMOR = Key.minecraft("armor");
+    public static final Key ARMOR_TOUGHNESS = Key.minecraft("armor_toughness");
+    public static final Key FALL_DAMAGE_MULTIPLIER = Key.minecraft("fall_damage_multiplier");
+    public static final Key LUCK = Key.minecraft("luck");
+    public static final Key MAX_ABSORPTION = Key.minecraft("max_absorption");
+    public static final Key SAFE_FALL_DISTANCE = Key.minecraft("safe_fall_distance");
+    public static final Key SCALE = Key.minecraft("scale");
+    public static final Key STEP_HEIGHT = Key.minecraft("step_height");
+    public static final Key GRAVITY = Key.minecraft("gravity");
+    public static final Key JUMP_STRENGTH = Key.minecraft("jump_strength");
+    public static final Key BURNING_TIME = Key.minecraft("burning_time");
+    public static final Key CAMERA_DISTANCE = Key.minecraft("camera_distance");
+    public static final Key EXPLOSION_KNOCKBACK_RESISTANCE = Key.minecraft("explosion_knockback_resistance");
+    public static final Key MOVEMENT_EFFICIENCY = Key.minecraft("movement_efficiency");
+    public static final Key OXYGEN_BONUS = Key.minecraft("oxygen_bonus");
+    public static final Key WATER_MOVEMENT_EFFICIENCY = Key.minecraft("water_movement_efficiency");
+    public static final Key TEMPT_RANGE = Key.minecraft("tempt_range");
+    public static final Key BLOCK_INTERACTION_RANGE = Key.minecraft("block_interaction_range");
+    public static final Key ENTITY_INTERACTION_RANGE = Key.minecraft("entity_interaction_range");
+    public static final Key BLOCK_BREAK_SPEED = Key.minecraft("block_break_speed");
+    public static final Key MINING_EFFICIENCY = Key.minecraft("mining_efficiency");
+    public static final Key SNEAKING_SPEED = Key.minecraft("sneaking_speed");
+    public static final Key SUBMERGED_MINING_SPEED = Key.minecraft("submerged_mining_speed");
+    public static final Key SWEEPING_DAMAGE_RATIO = Key.minecraft("sweeping_damage_ratio");
+    public static final Key SPAWN_REINFORCEMENTS = Key.minecraft("spawn_reinforcements");
+    public static final Key WAYPOINT_TRANSMIT_RANGE = Key.minecraft("waypoint_transmit_range");
+    public static final Key WAYPOINT_RECEIVE_RANGE = Key.minecraft("waypoint_receive_range");
+    public static final Key AIR_DRAG_MODIFIER = Key.minecraft("air_drag_modifier");
+    public static final Key FRICTION_MODIFIER = Key.minecraft("friction_modifier");
+    public static final Key BOUNCINESS = Key.minecraft("bounciness");
+    public static final Key BELOW_NAME_DISTANCE = Key.minecraft("below_name_distance");
+    public static final Key NAME_TAG_DISTANCE = Key.minecraft("name_tag_distance");
+
+
+    private Attribute() {
+    }
+}
