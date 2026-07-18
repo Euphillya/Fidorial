3a939fd1b95ead778ed32af4cf2ce28b9cb9624a
david <mail@nonswag.dev>
2026-07-19 00:42:11 +0200

    Add checkstyle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,10 @@ plugins {
 }
 
 subprojects {
-    apply(plugin = "java-library")
+    apply {
+        plugin("java-library")
+        plugin("checkstyle")
+    }
 
     group = "fr.euphyllia.fidorial"
     version = "0.1.0-SNAPSHOT"
@@ -25,5 +28,18 @@ subprojects {
     tasks.withType<JavaCompile>().configureEach {
         options.encoding = "UTF-8"
         options.release.set(25)
+    }
+
+    extensions.configure<CheckstyleExtension>("checkstyle") {
+        toolVersion = "13.8.0"
+        configDirectory.set(rootProject.layout.projectDirectory.dir("config"))
+        maxWarnings = 0
+    }
+
+    tasks.withType<Checkstyle>().configureEach {
+        reports {
+            xml.required.set(false)
+            html.required.set(true)
+        }
     }
 }
