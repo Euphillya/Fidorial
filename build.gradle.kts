0786610683ff3f37f20b9cdd552822480a421deb
david <mail@nonswag.dev>
2026-07-20 16:04:47 +0200

    add custom formatter rules
    - update import order
    - wrap declaration parameters
    - expand empty code blocks
    - separate module requires static

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,40 +1,17 @@
+import fr.euphyllia.fidorial.gradle.WrapDeclarationParameters
+
 plugins {
     id("java")
     id("com.diffplug.spotless") version "8.8.0"
 }
 
-spotless {
-    ratchetFrom("origin/master")
-
-    format("misc") {
-        target(
-            "**/*.gradle.kts",
-            "**/*.md",
-            "**/*.properties",
-            "**/*.json",
-            "**/*.toml",
-            "**/*.xml",
-            "**/*.yml",
-            "**/*.yaml",
-            ".github/workflows/*.yml",
-            ".github/workflows/*.yaml",
-        )
-        targetExclude(
-            ".gradle/**",
-            ".idea/**",
-            "build/**",
-            "**/build/**",
-            "**/run/**",
-        )
-        trimTrailingWhitespace()
-        endWithNewline()
-    }
+repositories {
+    mavenCentral()
 }
 
 subprojects {
     apply {
         plugin("java-library")
-        plugin("com.diffplug.spotless")
     }
 
     group = "fr.euphyllia.fidorial"
@@ -58,21 +35,47 @@ subprojects {
         options.encoding = "UTF-8"
         options.release.set(25)
     }
+}
 
-    extensions.configure<com.diffplug.gradle.spotless.SpotlessExtension> {
-        java {
-            ratchetFrom("origin/master")
-            target(
-                "**/*.java",
-            )
-            targetExclude(
-                ".gradle/**",
-                ".idea/**",
-                "build/**",
-                "run/**",
-            )
-            palantirJavaFormat("2.96.0")
-            formatAnnotations()
-        }
+spotless {
+    ratchetFrom("origin/master")
+
+    format("misc") {
+        target(
+            "*.gradle.kts",
+            "*.md",
+            "*.properties",
+            "*.json",
+            "*.toml",
+            "*.xml",
+            "*.yml",
+            "*.yaml",
+        )
+        targetExclude(
+            ".gradle/**",
+            ".idea/**",
+            "**/build/**",
+            "**/run/**",
+        )
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+
+    java {
+        target("**/*.java")
+
+        palantirJavaFormat("2.96.0")
+        forbidWildcardImports()
+        formatAnnotations()
+        removeUnusedImports()
+        custom("Wrap declaration parameters", WrapDeclarationParameters())
+        replaceRegex("Expand empty code blocks", """(?m)^([ \t]*)(.+) \{\}$""", "$1$2 {\n$1}")
+        replaceRegex(
+            "Separate module requires static",
+            """(?m)^(    requires (?!static )[^\n]+;\n)(    requires static )""",
+            "$1\n$2",
+        )
+
+        importOrder("", "javax|java", "\\#")
     }
 }
