a0717ca35e150c7a4220c1c5696a06a3171dfaba
Euphyllia Bierque <bierque.euphyllia@gmail.com>
2026-07-21 17:42:33 +0200

    Add docs for packet/clientbound/login

--- fidorial-server/src/main/java/fr/euphyllia/fidorial/server/protocol/packet/clientbound/login/ClientboundLoginFinishedPacket.java
+++ fidorial-server/src/main/java/fr/euphyllia/fidorial/server/protocol/packet/clientbound/login/ClientboundLoginFinishedPacket.java
@@ -5,6 +5,22 @@ import fr.euphyllia.fidorial.server.network.PacketBuffer;
 import fr.euphyllia.fidorial.server.protocol.catalog.LoginClientboundPackets;
 import fr.euphyllia.fidorial.server.protocol.packet.ClientboundPacket;
 
+/**
+ * <p><b>Packet ID:</b> Login = 2 (0x02)</p>
+ * <p><b>Source:</b> <a href="https://minecraft.wiki/w/Java_Edition_protocol/Packets#Login_Success">Login Success</a></p>
+ *
+ * <h4>Packet structure</h4>
+ * <table>
+ *   <caption>Fields, in write/read order</caption>
+ *   <thead>
+ *     <tr><th>#</th><th>Field</th><th>Type</th><th>Description</th></tr>
+ *   </thead>
+ *   <tbody>
+ *     <tr><td>0</td><td>Profile</td><td>Game Profile</td><td>&nbsp;</td></tr>
+ *     <tr><td>1</td><td>Session ID</td><td>UUID</td><td>&nbsp;</td></tr>
+ *   </tbody>
+ * </table>
+ */
 public record ClientboundLoginFinishedPacket(GameProfile profile) implements ClientboundPacket {
 
     @Override
