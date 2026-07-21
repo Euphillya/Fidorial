286e1c3cab14888f492fec2ca264f7918266601b
david <mail@nonswag.dev>
2026-07-21 18:52:16 +0200

    Document new API endpoints

--- fidorial-api/src/main/java/fr/fidorial/event/server/ServerStatusRequestEvent.java
+++ fidorial-api/src/main/java/fr/fidorial/event/server/ServerStatusRequestEvent.java
@@ -5,14 +5,30 @@ import fr.fidorial.status.ServerStatus;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Contract;
 
-
+/**
+ * Called when a client requests the server list status.
+ *
+ * @since 0.1.0
+ */
 @ApiStatus.NonExtendable
 public interface ServerStatusRequestEvent extends Event {
     // todo: expose client connection that requested the status
 
+    /**
+     * Gets the status that will be sent to the requesting client.
+     *
+     * @return current response status
+     * @since 0.1.0
+     */
     @Contract(pure = true)
     ServerStatus status();
 
+    /**
+     * Replaces the status that will be sent to the requesting client.
+     *
+     * @param status new response status
+     * @since 0.1.0
+     */
     @Contract(mutates = "this")
     void status(final ServerStatus status);
 }
