d39bff435ab1c991116ec694e339963ec52f0d6b
david <mail@nonswag.dev>
2026-07-23 20:41:29 +0200

    Remove unused EventExceptionHandler

--- fidorial-api/src/main/java/fr/fidorial/event/EventExceptionHandler.java
+++
@@ -1,6 +0,0 @@
-package fr.fidorial.event;
-
-@FunctionalInterface
-public interface EventExceptionHandler {
-    void handle(Throwable throwable, Class<?> eventClass, Subscription subscription);
-}
