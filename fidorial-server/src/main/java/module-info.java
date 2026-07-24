cf6a09db818fab1e6eca4e0c059b93c83b4f9748
david <mail@nonswag.dev>
2026-07-24 18:14:29 +0200

    Reorder module info

--- fidorial-server/src/main/java/module-info.java
+++ fidorial-server/src/main/java/module-info.java
@@ -5,6 +5,7 @@ module fr.fidorial.server {
     requires ch.qos.logback.classic;
     requires ch.qos.logback.core;
     requires com.google.common;
+    requires com.google.errorprone.annotations;
     requires com.google.gson;
     requires dev.faststats.config;
     requires dev.faststats;
@@ -12,24 +13,23 @@ module fr.fidorial.server {
     requires fr.fidorial;
     requires io.netty.buffer;
     requires io.netty.codec;
+    requires io.netty.transport.classes.epoll;
+    requires io.netty.transport.classes.io_uring;
+    requires io.netty.transport.classes.kqueue;
     requires io.netty.transport.unix.common;
     requires io.netty.transport;
+    requires java.management;
     requires net.kyori.adventure.api;
     requires net.kyori.adventure.key;
     requires net.kyori.adventure.text.logger.slf4j;
     requires net.kyori.adventure.text.minimessage;
     requires net.kyori.adventure.text.serializer.ansi;
     requires net.kyori.adventure.text.serializer.gson;
-    requires org.slf4j;
+    requires net.kyori.adventure.text.serializer.plain;
     requires org.jline.reader;
     requires org.jline.terminal;
+    requires org.slf4j;
 
     requires static org.jetbrains.annotations;
     requires static org.jspecify;
-    requires com.google.errorprone.annotations;
-    requires net.kyori.adventure.text.serializer.plain;
-    requires io.netty.transport.classes.epoll;
-    requires io.netty.transport.classes.kqueue;
-    requires io.netty.transport.classes.io_uring;
-    requires java.management;
 }
