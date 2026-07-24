ec84118f972193eba5454af5c0748db33c2d79e8
David <mail@nonswag.dev>
2026-07-24 23:29:38 +0200

    Cleanup gradle configuration (#28)
    * Remove unnecessarry wrapper module to allow unnamed module consumption
    
    * Require module name
    
    * Cleanup dependencies
    
    * Reorder module info
    
    * Move version to gradle.properties
    
    * read unnamed modules from all modules
    
    * apply java plugin to all modules
    
    * Make group name consistent with rest of the project
    
    * Move faststats repo to server module
    
    * Properly define unnamed module names read
    
    * simplify run task configuration
    
    * fix failing javadocJar

--- fidorial-server/build.gradle.kts
+++ fidorial-server/build.gradle.kts
@@ -1,39 +1,39 @@
+extra.set("readUnnamedModules", setOf("fr.fidorial", "fr.fidorial.server"))
+
 plugins {
     application
     id("com.gradleup.shadow")
 }
 
+repositories {
+    maven("https://repo.faststats.dev/releases")
+}
+
 dependencies {
-    implementation(projects.fidorialApi)
-    implementation(projects.fidorialAuth)
-    implementation(libs.netty.all)
-    implementation(libs.gson)
-    implementation(libs.slf4j.api)
     implementation(libs.faststats.config)
     implementation(libs.faststats.core)
-    implementation(libs.adventure.text.serializer.ansi)
-    implementation(libs.logback.classic)
-    implementation(libs.adventure.text.serializer.plain)
     implementation(libs.jline.ffm)
     implementation(libs.jline.reader)
+    implementation(libs.logback.classic)
+    implementation(libs.netty.all)
+    implementation(projects.fidorialApi)
+    implementation(projects.fidorialAuth)
     runtimeOnly(libs.netty.epoll)
-    runtimeOnly(libs.netty.kqueue)
     runtimeOnly(libs.netty.iouring)
+    runtimeOnly(libs.netty.kqueue)
 }
 
 application {
     mainClass.set("fr.euphyllia.fidorial.server.Main")
 }
 
-tasks.named<JavaExec>("run") {
+tasks.run {
     description = "Spin up a test server without assembling a jar"
     standardInput = System.`in`
     classpath(sourceSets.main.map { it.runtimeClasspath })
     workingDir = project.file("run")
     workingDir.mkdirs()
-}
-
-tasks.named("run") {
+    jvmArgs = listOf("--enable-native-access=ALL-UNNAMED")
     dependsOn(":fidorial-test-plugin:build")
 }
 
