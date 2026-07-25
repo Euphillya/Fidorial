5ac1fd4bf69da6bbf5e0483287ce97914f85a71e
david <mail@nonswag.dev>
2026-07-25 18:02:35 +0200

    Move and rename `Dependency#shareClasspath` to `PluginDependency#joinClasspath`

--- fidorial-api/src/main/java/fr/fidorial/plugin/PluginMeta.java
+++ fidorial-api/src/main/java/fr/fidorial/plugin/PluginMeta.java
@@ -43,6 +43,9 @@ public interface PluginMeta {
         // input: ~1.2.3 equivalent: >=1.2.3 <1.3.0
         // input: ^1.2.3 equivalent: >=1.2.3 <2.0.0
         String versionRange();
+
+        boolean joinClasspath();
+
         RelativeLoadOrder load();
 
         enum RelativeLoadOrder {
@@ -72,8 +75,6 @@ public interface PluginMeta {
     }
 
     interface Dependency {
-        boolean shareClasspath();
-
     }
 
     interface PermissionEntry {
