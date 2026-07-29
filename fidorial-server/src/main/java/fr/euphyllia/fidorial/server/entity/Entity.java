b12cedf78d4d8d5345ca06b3f5238fad3819fe9f
Toffikk <slodkitofik@gmail.com>
2026-07-29 11:01:30 +0200

    Fix playSound API

--- fidorial-server/src/main/java/fr/euphyllia/fidorial/server/entity/Entity.java
+++ fidorial-server/src/main/java/fr/euphyllia/fidorial/server/entity/Entity.java
@@ -1,4 +1,8 @@
 package fr.euphyllia.fidorial.server.entity;
 
-public interface Entity {
+import net.kyori.adventure.sound.Sound;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.event.HoverEventSource;
+
+public interface Entity extends HoverEventSource<HoverEvent.ShowEntity>, Sound.Emitter, Sound.Source.Provider {
 }
