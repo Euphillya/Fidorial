7f4d4ab1769018a0ec02afff2cc2c5db78ca35fb
Toffikk <slodkitofik@gmail.com>
2026-07-29 11:05:15 +0200

    Revert "Fix playSound API"
    This reverts commit b12cedf78d4d8d5345ca06b3f5238fad3819fe9f.

--- fidorial-server/src/main/java/fr/euphyllia/fidorial/server/entity/Entity.java
+++ fidorial-server/src/main/java/fr/euphyllia/fidorial/server/entity/Entity.java
@@ -1,8 +1,4 @@
 package fr.euphyllia.fidorial.server.entity;
 
-import net.kyori.adventure.sound.Sound;
-import net.kyori.adventure.text.event.HoverEvent;
-import net.kyori.adventure.text.event.HoverEventSource;
-
-public interface Entity extends HoverEventSource<HoverEvent.ShowEntity>, Sound.Emitter, Sound.Source.Provider {
+public interface Entity {
 }
