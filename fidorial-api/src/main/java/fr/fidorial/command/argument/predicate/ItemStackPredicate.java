834f75da98be635a87e93c9761b4a56f982f1c21
Toffikk <slodkitofik@gmail.com>
2026-08-06 22:24:36 +0200

    javadoc more

--- fidorial-api/src/main/java/fr/fidorial/command/argument/predicate/ItemStackPredicate.java
+++ fidorial-api/src/main/java/fr/fidorial/command/argument/predicate/ItemStackPredicate.java
@@ -5,6 +5,12 @@ import org.jetbrains.annotations.ApiStatus;
 
 import java.util.function.Predicate;
 
+/**
+ * A {@link Predicate} matching {@link ItemStack}s, produced by
+ * parsing an item predicate argument.
+ *
+ * @since 0.1.0
+ */
 @ApiStatus.NonExtendable
 public interface ItemStackPredicate extends Predicate<ItemStack> {
 }
