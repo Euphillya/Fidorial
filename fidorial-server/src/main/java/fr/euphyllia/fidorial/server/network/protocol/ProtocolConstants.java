29d3cba4d3b4f8c6b38d0bff9e4340bf19e8816d
Toffikk <slodkitofik@gmail.com>
2026-08-31 22:12:09 +0200

    Initial protocol update pass

--- fidorial-server/src/main/java/fr/euphyllia/fidorial/server/network/protocol/ProtocolConstants.java
+++ fidorial-server/src/main/java/fr/euphyllia/fidorial/server/network/protocol/ProtocolConstants.java
@@ -2,8 +2,8 @@ package fr.euphyllia.fidorial.server.network.protocol;
 
 public final class ProtocolConstants {
 
-    public static final String MINECRAFT_VERSION = "26.2";
-    public static final int PROTOCOL_VERSION = 776;
+    public static final String MINECRAFT_VERSION = "26.3-snapshot-10";
+    public static final int PROTOCOL_VERSION = 1073742156;
     public static final int MAX_PACKET_SIZE = 2 * 1024 * 1024;
     public static final int COMPRESSION_THRESHOLD = 256;
     public static final int MAX_NBT_LENGTH = 0xFFFF;
